f7452ac6c599b7cc0fa0334787a89f404985c9d4
ZMantas <60478106+ZMantass@users.noreply.github.com>
2020-03-09 09:56:59 +0000

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static bool FileEquals(string path1, string path2)
+    {
+        byte[] file1 = File.ReadAllBytes(path1);
+        byte[] file2 = File.ReadAllBytes(path2);
+        if (file1.Length == file2.Length)
+        {
+            for (int i = 0; i < file1.Length; i++)
+            {
+                if (file1[i] != file2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FileDiff
+{
+    class FileInput
+    {
+        static void Main()
+        {
+            bool a = FileEquals("C:\\stage\\htmlmeta",
+                "C:\\stage\\htmlmeta-aspnet");
+
+            bool b = FileEquals("C:\\stage\\htmllink",
+                "C:\\stage\\htmlmeta-aspnet");
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+
+}
+
